ca6d0ef6fe8c4fd5466742e34bcff5c3729298dc
Zara <24zalam@athenian.org>
2021-09-17 06:45:04 -0700

    the average person falls asleep in 7 minutes

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,31 +1,34 @@
 public class MyMain {
 
     // Changes a capital letter to a lowercase letter
-    // If the char is not an uppercase letter, 
-    // it is returned unchanged. 
+    // If the char is not an uppercase letter,
+    // it is returned unchanged.
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        char lower=Character.toLowerCase(ch);
+        return lower;
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        char upper=Character.toUpperCase(ch);
+        return upper;
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+       str= str.replaceAll("[0-9]","");
+       return str;
+
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println(removeNumbers("z0ra123"));
+        System.out.println(toUpper('G'));
+        System.out.println(toLower('G'));
     }
 }
